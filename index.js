ce6510daee691d6b6879f37f1b24c65b30db9f7f
rhry05 <83847748+rhry05@users.noreply.github.com>
2021-06-05 12:02:18 +0900

    im so tired

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var diary_num = 3;
+var diary_idx = 1;
+
+
 window.onload = function () {
     var desc = [document.getElementById('book1_desc').innerText,"ㄹㅇㅋㅋ","ㅋㅋ"];
     var index = 0;
@@ -51,5 +55,49 @@ rightarrow.onclick = function () {
     
     })
     }
+    replaceDiary('/diary/diary1.html');
 
+    var diary_left_arrow = document.getElementById("diary_left_arrow");
+    var diary_right_arrow = document.getElementById("diary_right_arrow");
+
+    diary_left_arrow.onclick = diaryLeftClick;
+    diary_right_arrow.onclick = diaryRightClick;
+}
+
+function replaceDiary(filename) {
+    $.get(filename, function(result){
+        var obj = $(result).find('p');
+        document.getElementById("diary_text").innerText = obj.html();
+
+    });
+    $.get(filename, function(result){
+        var obj = $(result).find('h2');
+        document.getElementById("diary_title").innerText = obj.html();
+
+    });
+    $.get(filename, function(result){
+        var obj = $(result).find('img');
+        document.getElementById("diary_img").src = obj[0].src;
+
+    });
+}
+
+
+function diaryRightClick(){
+    $('.diary').fadeOut(500, function(){
+        diary_idx++;
+        if (diary_idx > diary_num) diary_idx = 1;
+        var html = "/diary/diary" +diary_idx + ".html";
+        replaceDiary(html);
+        $(this).fadeIn(500);
+    });
+}
+function diaryLeftClick(){
+    $('.diary').fadeOut(500, function(){
+        diary_idx--;
+        if (diary_idx < 1) diary_idx = diary_num;
+        var html = "/diary/diary" +diary_idx + ".html";
+        replaceDiary(html);
+        $(this).fadeIn(500);
+    });
 }
